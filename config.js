f23ae0a823fc3796d808c00e3f8e9914a744c04f
Everton Fontes <efs.fontes@gmail.com>
2023-03-09 13:33:35 -0300

    security

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ import {config} from 'dotenv'
 config()
 
 const conf = {
-    hostEmail: process.env.EMAILHOST || "smtp.gmail.com",
+    hostEmail: process.env.EMAILHOST,
     portEmail: Number(process.env.EMAILPORT) || 587,
-    usernameEmail: process.env.EMAILUSERNAME || "efs.fontes@gmail.com",
-    passwordEmail: process.env.EMAILPASSWORD || "osdgmckyamzakolj",
-    senderEmail: process.env.EMAILSENDER || "efs.fontes@gmail.com",
+    usernameEmail: process.env.EMAILUSERNAME,
+    passwordEmail: process.env.EMAILPASSWORD,
+    senderEmail: process.env.EMAILSENDER,
     isGmail: Boolean(process.env.EMAILGMAIL) || false,
     port: process.env.PORT || 3000
 }
